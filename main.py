787e21940c985e77913c2892642977c4094090e7
crazy304 <100753851+rich304@users.noreply.github.com>
2022-03-08 10:21:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print('Spēle: Elektriskais rallijs 2022')
+print('Autori: Krists Apinis un Ričards Ābols')
+print('Spēles noteikumi: Spēle sākas pie VVĢ, sausā laikā ar auto vari nobraukt 140km, lietainā laikā 80km, pirms apstāšanās priekš uzlādes. Kopumā ir 12 kontrolpunkti ieskaitot sākumu (VVĢ), un beigas (Liepāja) kuri  ir apskates vietas. Apstāšanās pie kontrolpunktiem ir obligāta. Kontrolpunktos tev ir jāatbild uz vienu jautājumu lai tiktu tālāk. Uzpildīties varēsi paceļam Circle K izveidotajās uzpildes stacijās')
